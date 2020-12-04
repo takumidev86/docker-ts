f576d3c021f61dcacb64b3eff5831e974e4f05fc
takumidev86 <takumidevelopment86@gmail.com>
2020-12-05 02:40:27 +0900

    [Fix] App.tsx hooks

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+interface Props {}
+
+const App: React.FC<Props> = (props) => {
+  const [count, setCount] = useState<number>(0);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +14,15 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        {count}
+        <a
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          add
+        </a>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,6 +34,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;

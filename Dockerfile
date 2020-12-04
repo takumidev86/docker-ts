3e603a0f409bf90a7d4bafcbc6720014ff73df68
takumidev86 <takumidevelopment86@gmail.com>
2020-12-05 01:19:09 +0900

    [Add] eslint

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,6 @@
 FROM node:12.20.0-alpine3.9
+COPY ./front ./front
+WORKDIR /front
+RUN npm install
+EXPOSE 4000
+CMD npm start
---
+++ front/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    'jest/globals': true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'jest'
+  ],
+  rules: {
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error']
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+}
